242f47c3757a1484c40e593b8f39fd2ee435f7ae
John Napiorkowski <johnn@John-Napiorkowski-MacBook-Pro.local>
2010-07-02 11:56:40 -0400

    makefile and pod

---
+++ lib/Shutterstock/Example.pm
@@ -0,0 +1,25 @@
+package Shutterstock::Example;
+ 
+use 5.008008;
+our $VERSION = '0.01';
+ 
+=head1 NAME
+
+Shutterstock::Example - An Example application for Internal Shutterstock IT
+
+=head1 DESCRIPTION
+
+Example Code
+
+=head1 AUTHOR
+
+Shutterstock Coders C<< <coders@shutterstock.com> >>
+
+=head1 LICENSE & COPYRIGHT
+
+Copyright 2009, Shutterstock, LLC
+proprietary code not for distribution
+
+=cut
+
+1;
