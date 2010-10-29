9a2d43651320d566aa110ef86b68415b46497a4e
John Napiorkowski <jjn1056@yahoo.com>
2010-10-28 23:13:31 -0400

    more prep for release

--- lib/Shutterstock/Example.pm
+++ lib/Shutterstock/Example.pm
@@ -25,19 +25,39 @@ either as a developer or as an administrator.
 Developers are primarily interested in ability to checkout code, make and share
 code changes, run tests, try out new dependencies and resolve merge conflicts
 with other developers.  Suggested method is for a developer to install a user
-level Perl with L<App::Perlbrew>, bootstrap a developer kit (for now the easy
+level Perl with L<App::perlbrew>, bootstrap a developer kit (for now the easy
 way to do this is via L<Task::BeLike::JJNAPIORK>), clone the git repository and
 then install dependencies.  Checklist follows:
 
 =over4
 
-=item install L<App::Perlbrew>
+=item install L<App::perlbrew>
+
+Just follow the basic instructions to setup a user level Perl executable from
+the L<App::perlbrew> documentation.
 
 =item bootstrap SDK and L<local::lib>
 
+The first command bootstraps the SDK for Modern Perl development, the second
+enables your L<local::lib> in C<$HOME/extlib> so that you can use it.
+
+    * curl -L http://cpanmin.us/ | perl - -l $HOME/extlib Task::BeLike::JJNAPIORK
+    * $HOME/extlib/bin/localenv bash
+
+See the documentation for L<App::local::lib::helper> on the C<localenv> and the
+C<localenv-bashrc> scripts.
+
 =item clone git repository
 
+Fork http://github.com/jjn1056/Shutterstock-Example and clone it.
+
 =item install dependencies
+
+    cpanm --installdeps .
+
+=item start the server
+
+    plackup -Ilib ./script/shutterstock-example-web.psgi
 
 =back
 
@@ -48,11 +68,21 @@ to all target servers and HUP application servers.  Checklist follows:
 
 =item create tarball
 
+    * perl Makefile.PL
+    * make test
+    * make dist
+
 =item create new installation from tarball
+
+    * cpanm -l $HOME/local-v0.08 Shutterstock-Example-0.08.tar.gz 
 
 =item sync across target servers
 
+    TBD
+
 =item HUP applications servers
+
+    TBD
 
 =head1 AUTHOR
 
--- t/use.t
+++ t/use.t
@@ -1,7 +1,4 @@
-use strict; 
-use warnings; 
-use Test::More; 
- 
+use Test::Most 'bail';  
 use_ok 'Shutterstock::Example::Web'; 
  
 done_testing; 
