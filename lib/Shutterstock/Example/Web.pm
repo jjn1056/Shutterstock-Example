cea282676ea24733bd797b0ebc0a0fc7e1936680
John Napiorkowski <jjn1056@yahoo.com>
2010-11-04 23:39:12 -0400

    step toward crud, but ugly crud

--- lib/Shutterstock/Example/Web.pm
+++ lib/Shutterstock/Example/Web.pm
@@ -10,21 +10,42 @@ dispatch {
     sub (/) {
         $self->show_landing;
     },
+    sub (/users) {
+        my($self, $env) = @_[0, +PSGI_ENV];
+        $self->show_users(WebSchema($env)->resultset('User'));
+    },
     subdispatch sub (/user) {
         my($self, $env) = @_[0, +PSGI_ENV];
         my $form = Shutterstock::Example::Web::User->new(schema=>WebSchema($env));
         [
             sub (GET) {
-                $self->show_new_user_form($form);
+                $self->show_user_form($form);
             },
-            sub (POST + %:email=&*) {
+            sub (POST + %:email=&:@roles~) {
                 my ($self, $env1, $env2, $params) = @_;
                 $form->process(params => $params) ?
                   $self->show_you_create_a_user :
-                  $self->show_new_user_form($form);
-            }
+                  $self->show_user_form($form);
+            },
         ],
     },
+    subdispatch sub (/user/*) {
+        my($self, $id, $env) = @_[0, 3, +PSGI_ENV];
+        my $item = WebSchema($env)->resultset('User')->find({user_id=>$id});
+        my $form = Shutterstock::Example::Web::User->new(item=>$item);
+        [
+            sub (GET) {
+                $self->show_user_form($form);
+            },
+            sub (POST + %:email=&:@roles~) {
+                my ($self, $env1, $env2, $env3, $params) = @_;
+                $form->process(params => $params) ?
+                  $self->show_you_create_a_user :
+                  $self->show_user_form($form);
+            },
+        ]
+
+    }
 };
 
 sub as_html {
@@ -69,22 +90,63 @@ sub show_landing {
         <p>, "Hi, I'm version: ", $data{site_version}, </p>,
         <p>, "Here's some interesting things about me", </p>,
         <ul>,
-            <li>, <a href="/user">, "My Users", </li>,
+            <li>, <a href="/users">, "My Users", </li>,
+            <li>, <a href="/user">, "Create New User", </li>,
         </ul>;
     }
 
-sub show_new_user_form {
+sub show_users {
+    my ($self, $user_rs) = @_;
+    as_html(\&users, (
+        title => "All Users",
+        user_rs => $user_rs,
+    ));
+}
+
+    sub users {
+        my (%data) = @_;
+        map({
+            my $id = $_->user_id;
+            <p>, <a href="/user/$id">, $_->email, </a>, </p>
+        } $data{user_rs}->all),
+        <a href="/">, "Home", </a>;
+    }
+
+sub show_user_form {
     my ($self, $form) = @_;
-    as_html(\&new_user_form, (
+    as_html(\&user_form, (
         title => "Create a User",
         form => $form,
     ));
 }
 
-    sub new_user_form {
+    sub user_form {
         my (%data) = @_;
-        my $form = $data{form}->render;
-        return <div>, \$form, </div>;
+        my $email = $data{form}->field('email')->value || '';
+        my @roles = $data{form}->field('roles')->options;
+        my @email_errors = @{$data{form}->field('email')->errors};
+        my $role_id = $data{form}->field('roles')->value;
+        my @role_ids = ref $role_id ? @$role_id : $role_id;
+
+        <form id="user-form" method="post">,
+          <fieldset>,
+            <div>,
+              <label class="form-label" for="email">, "Email: ", </label>,
+              <input type="text" name="email" id="email" value="$email" />,
+              map({
+              <span class="error_message">, " $_", </span>
+              } @email_errors), 
+            </div>,
+            map({
+              my ($value, $label) = @{$_}{qw/value label/};
+              my $checked = grep({ warn $_, $value; $_ eq $value } @role_ids) ? "checked" : "";
+              <input type="checkbox" name="roles" value="$value" $checked />, $label, <br/>
+            } @roles),
+            <div>,
+              <input type="submit" name="submit" id="submit" value="Save" />,
+            </div>,
+          </fieldset>,
+        </form>;
     }
 
 sub show_you_create_a_user {
@@ -92,9 +154,9 @@ sub show_you_create_a_user {
 }
 
     sub you_created_a_user {
-        <p>, "You made a user!", </p>;
+        <p>, "You made a user!", </p>,
+        <a href="/">, "Home", </a>;
     }
 
 1;
 
-   
